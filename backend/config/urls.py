54ad8a1c72b42aca41974dd227b30c61c0f34418
Jefferson Bruchado <jefferson.damaceno17@gmail.com>
2018-12-10 20:07:59 -0200

    adding user app to urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,20 @@
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('src.v1.user.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
