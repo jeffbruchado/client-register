1f8d2df69a4c2fdd6c113bd446de95ef1fa935c3
Jefferson Bruchado <jefferson.damaceno17@gmail.com>
2018-12-11 07:06:06 -0200

    registering user abstract model

--- backend/src/v1/user/admin.py
+++ backend/src/v1/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models.user import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    exclude = ('groups', 'user_permissions')
+
+
+admin.site.register(User, UserAdmin)
