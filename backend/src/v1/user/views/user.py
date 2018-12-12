7d40b6647388cb8315c69445acffb37a680e9b97
Jefferson Bruchado <jefferson.damaceno17@gmail.com>
2018-12-12 19:26:07 -0200

    adding hash generation based in cpf

--- backend/src/v1/user/views/user.py
+++ backend/src/v1/user/views/user.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from src.v1.user.serializers.user import UserSerializerCreate
 from rest_framework.authtoken.models import Token
+import hashlib
 
 
 class UserView(APIView):
@@ -17,8 +18,11 @@ class UserView(APIView):
             user = serializer.save()
             if user:
                 token = Token.objects.create(user=user)
+                hash_object = hashlib.sha256(request.data.get('cpf').encode())
+                hex_dig = hash_object.hexdigest()
                 json = serializer.data
                 json['token'] = token.key
+                json['hash'] = hex_dig
                 return Response(json, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
